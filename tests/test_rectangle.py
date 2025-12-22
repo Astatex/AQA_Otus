b07cc8ae69c26e6bc77510ade62fdcfce8f4a4aa
Astatex <astatex57@gmail.com>
2025-12-22 22:43:30 +0300

    Коммит с комментариями, как можно использовать параметризацию в тесте с add_area

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -4,29 +4,35 @@ from src.rectangle import Rectangle
 from src.square import Square
 from src.triangle import Triangle
 
-def test_add_aria_positive_rectangle_and_square():
-    r = Rectangle(3, 5)
-    s = Square(5)
-    assert s.add_area(r) == 40, "Сумма площадей прямоугольника и квадрата"
 
-@pytest.mark.parametrize('non_figure',
-    [
-        -3, 42, 'abc'
-    ]
- )
+@pytest.mark.parametrize(
+    "figure_1, figure_2, expected_result",
+    [(Rectangle(3, 5), Square(5), 40), (Circle(3), Square(3), 37.27)],
+    ids=[
+        "Прямоугольник и Квадрат",
+        "Круг и Квадрат",
+    ],
+)
+def test_add_aria_positive(figure_1, figure_2, expected_result):
+    assert figure_1.add_area(figure_2) == expected_result, f"Ожидали {expected_result}"
 
+
+# def test_add_aria_positive_rectangle_and_square():
+#     r = Rectangle(3, 5)
+#     s = Square(5)
+#     assert s.add_area(r) == 40, "Сумма площадей прямоугольника и квадрата"
+
+
+@pytest.mark.parametrize("non_figure", [-3, 42, "abc"])
 def test_add_area_negative_non_figure(non_figure):
     r = Rectangle(3, 5)
     with pytest.raises(ValueError, match="Должна быть фигура"):
         r.add_area(non_figure)
 
-@pytest.mark.parametrize(
-    ('side_a', 'aria', 'perimeter'),
-    [
-        (3, 28.274333882308138, 18.84955592153876)
-    ]
-)
 
+@pytest.mark.parametrize(
+    ("side_a", "aria", "perimeter"), [(3, 28.274333882308138, 18.84955592153876)]
+)
 def test_circle_positive(side_a, aria, perimeter):
     c = Circle(side_a)
     assert c.get_area() == aria, f"Площадь круга с радиусом {side_a}"
@@ -42,13 +48,8 @@ def test_circle_negative():
         "Периметр круга с радиусом 3 должен равняться 18.84955592153876"
     )
 
-@pytest.mark.parametrize(
-"radius",
-    [
-        0, -3
-    ]
-)
 
+@pytest.mark.parametrize("radius", [0, -3])
 def test_circle_input_error(radius):
     with pytest.raises(ValueError):
         Circle(radius)
@@ -69,13 +70,8 @@ def test_rectangle_negative():
         "Периметр прямоугольника со сторонами 3 и 5 должна равняться 16"
     )
 
-@pytest.mark.parametrize(
-"side",
-    [
-        0, -3
-    ]
-)
 
+@pytest.mark.parametrize("side", [0, -3])
 def test_rectangle_input_error(side):
     with pytest.raises(ValueError, match="Стороны прямоугольника должны быть больше 0"):
         Rectangle(3, side)
@@ -94,37 +90,29 @@ def test_square_negative():
         "Периметр квадрата со сторонами 3 должна равняться 12"
     )
 
-@pytest.mark.parametrize(
-"side",
-    [
-        0, -3
-    ]
-)
 
+@pytest.mark.parametrize("side", [0, -3])
 def test_square_input_error(side):
     with pytest.raises(ValueError, match="Стороны квадрата должны быть больше 0"):
         Square(side)
 
 
 @pytest.mark.parametrize(
-    ('side_a', 'site_b', 'side_c', 'aria', 'perimeter'),
-    [
-        (13, 14, 15, 84, 42)
-    ]
+    ("side_a", "site_b", "side_c", "aria", "perimeter"), [(13, 14, 15, 84, 42)]
 )
-
 def test_triangle_positive(side_a, site_b, side_c, aria, perimeter):
     s = Triangle(side_a, site_b, side_c)
-    assert s.get_area() == aria, f"Площадь треугольника со сторонами {side_a}, {site_b} и {side_c}"
-    assert s.get_perimeter() == perimeter, f"Площадь треугольника со сторонами {side_a}, {site_b} и {side_c}"
+    assert s.get_area() == aria, (
+        f"Площадь треугольника со сторонами {side_a}, {site_b} и {side_c}"
+    )
+    assert s.get_perimeter() == perimeter, (
+        f"Площадь треугольника со сторонами {side_a}, {site_b} и {side_c}"
+    )
+
 
 @pytest.mark.parametrize(
-    ('side_a', 'site_b', 'side_c', 'aria', 'perimeter'),
-    [
-        (13, 14, 15, 84, 42)
-    ]
+    ("side_a", "site_b", "side_c", "aria", "perimeter"), [(13, 14, 15, 84, 42)]
 )
-
 def test_triangle_negative(side_a, site_b, side_c, aria, perimeter):
     s = Triangle(side_a, site_b, side_c)
     assert s.get_area() != 85.0, (
@@ -134,13 +122,8 @@ def test_triangle_negative(side_a, site_b, side_c, aria, perimeter):
         f"Площадь треугольника со сторонами {side_a}, {site_b} и {side_c} должна равняться {perimeter}"
     )
 
-@pytest.mark.parametrize(
-"side",
-    [
-        0, -3
-    ]
-)
 
+@pytest.mark.parametrize("side", [0, -3])
 def test_triangle_input_error(side):
     with pytest.raises(ValueError, match="Стороны треугольника должны быть больше 0"):
         Triangle(side, 14, 15)
